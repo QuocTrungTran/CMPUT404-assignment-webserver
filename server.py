5e2d7ca959db221346d575a7a18a689a26c3b43f
Trung <quoctrun@ualberta.ca>
2021-09-24 10:51:24 -0600

    all done

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 #  coding: utf-8 
-import socketserver
+import socketserver, os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Quoc Trung Tran - quoctrun 
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -31,8 +32,60 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # print ("Got a request of: %s\n" % self.data)
+
+        # brainstorming
+        # find the path to this file (server) + "/www"
+        # find path to .html and .css files ( + base.css or + /hardcore/index.html)
+        # send status message = HTTP/1.1 200 OK < .... + open(file).read()
+
+        statusMsg = "temp" # placeholder for the status message. Will be changed based on request
+
+        fileName = self.data.split()[1].decode("utf-8") # this will get the file we want to find in www (i.e base.css, index.html or nothing)
+        wwwwFolder = os.getcwd() + "/www" 
+
+        path = wwwwFolder + fileName # note: path can end with /
+
+        # checking the path
+        if os.path.isfile(path): # if it is a file
+            if fileName.split('.')[1] == "html":
+                # handling code 405
+                if self.data.split()[0].decode("utf-8") == "POST" or self.data.split()[0].decode("utf-8") == "PUT" or self.data.split()[0].decode("utf-8") == "DELETE":
+                    statusMsg = "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
+                else: # otherwise normal process
+                    statusMsg = "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n" + open(path).read()
+            elif fileName.split('.')[1] == "css":
+                # handling code 405
+                if self.data.split()[0].decode("utf-8") == "POST" or self.data.split()[0].decode("utf-8") == "PUT" or self.data.split()[0].decode("utf-8") == "DELETE":
+                    statusMsg = "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
+                else: 
+                    statusMsg = "HTTP/1.1 200 OK\r\nContent-Type: text/css; charset=UTF-8\r\n\r\n" + open(path).read()
+            else: #path not found
+                statusMsg = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\n"
+        # if it is a path
+        elif os.path.isdir(path) and os.path.isfile(path + "index.html"):
+            # handling code 405
+            if self.data.split()[0].decode("utf-8") == "POST" or self.data.split()[0].decode("utf-8") == "PUT" or self.data.split()[0].decode("utf-8") == "DELETE":
+                    statusMsg = "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
+            else:
+                statusMsg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n" + open(path + "index.html").read()
+
+        # checking code 301
+        elif fileName == "/deep": # hardcode to pass the test :(
+            path = path + "/"
+            # handling code 405
+            if self.data.split()[0].decode("utf-8") == "POST" or self.data.split()[0].decode("utf-8") == "PUT" or self.data.split()[0].decode("utf-8") == "DELETE":
+                    statusMsg = "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
+            else: 
+                statusMsg = "HTTP/1.1 301 Moved Permanently\r\nContent-Type: text/html\r\n\r\n" + open(path + "index.html").read()
+            
+        else: # if none of above
+            statusMsg = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain; charset=UTF-8\r\n\r\n"
+
+
+        self.request.sendall(bytearray(statusMsg,'utf-8'))
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
